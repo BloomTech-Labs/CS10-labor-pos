d68fc667286c80a872d45e4f878dda57632f8e91
Dre'Sean Williams <williamsdresean@gmail.com>
2018-09-11 16:49:55 -0400

    added all fields to contractor, needs auth and defs

--- POSserver/server/models/contractor.py
+++ POSserver/server/models/contractor.py
@@ -12,4 +12,7 @@ class Contractor(models.User):
     password = models.CharField(max_length=50, null=False, blank=False)
     security_question = models.TextField(blank=False, null=False)
     hashed_security_answer = models.TextField(blank=False, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    modified_at = models.DateTimeField(auto_now=True, blank=False, null=False)
+    premium = models.BooleanField(default=False, blank=False, null=False)
+    paid_until = DateTimeField(default=timezone.now, blank=True, null=True)
