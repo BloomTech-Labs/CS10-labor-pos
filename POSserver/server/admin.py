697271550fb072ef3233a4950067ff05abe0ff23
Zach Campbell <beazlebubbles@gmail.com>
2018-09-11 15:50:32 -0600

    Add account_job model Fix bug with duplicate INSTALLED_APPS

--- POSserver/server/admin.py
+++ POSserver/server/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tag, Job_Note
+from .models import Tag, Job_Note, Account_Job
+
 
 # Register your models here.
 class Tag_Admin(admin.ModelAdmin):
@@ -10,5 +11,10 @@ class Job_Note_Admin(admin.ModelAdmin):
     pass
 
 
+class Account_Job_Admin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Tag, Tag_Admin)
 admin.site.register(Job_Note, Job_Note_Admin)
+admin.site.register(Account_Job, Account_Job_Admin)
