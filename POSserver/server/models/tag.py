9bb9b11bdbe658e5f593c0f7002e083fc6a44e1f
Zach Campbell <beazlebubbles@gmail.com>
2018-09-11 11:55:09 -0600

    Move server directory inside POS_server Add tag model

---
+++ POSserver/server/models/tag.py
@@ -0,0 +1,10 @@
+import uuid
+from django.db import models
+
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
