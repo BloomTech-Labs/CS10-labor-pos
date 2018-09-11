e7d7f16cf88c6faed686d3af6349d85fe30dca12
Cole Phillips <nphillips78@gmail.com>
2018-09-11 16:16:38 -0400

    in order to not lose things when I pull the changes Amy made

--- POSserver/server/models/account.py
+++ POSserver/server/models/account.py
@@ -1,5 +1,85 @@
 from django.db import models
-
+from uuid import uuid4
 
 class Account(models.Model):
-    pass
+    id = models.UUIDField(primary-key=True, default=uuid4, editable=False, null=False)
+    business_name = models.CharField(max_length=100, null=False, blank=False)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=70, null=False, blank=False)
+    street_address = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=70, null=False, blank=False)
+    state_choices = (
+        ('AL', 'Alabama'),
+        ('AK', 'Alaska'),
+        ('AZ', 'Arizona'),
+        ('AR','Arkansas'),
+        ('CA','California'),
+        ('CO','Colorado'),
+        ('CT','Connecticut'),
+        ('DC','Distric of Columbia'),
+        ('DE','Delaware'),
+        ('FL','Florida'),
+        ('GA','Georgia'),
+        ('HI','Hawaii'),
+        ('ID','Idaho'),
+        ('IL','Illinois'),
+        ('IN','Indiana'),
+        ('IA','Iowa'),
+        ('KS','Kansas'),
+        ('KY','Kentucky'),
+        ('LA','Louisiana'),
+        ('ME','Maine'),
+        ('MD','Maryland'),
+        ('MA','Massachusettes'),
+        ('MI','Michigan'),
+        ('MN','Minnesota'),
+        ('MS','Mississippi'),
+        ('MO','Missouri'),
+        ('MT','Montana'),
+        ('NE','Nebraska'),
+        ('NV','Nevada'),
+        ('NH','New Hampshire'),
+        ('NJ','New Jersey'),
+        ('NM','New Mexico'),
+        ('NY','New York'),
+        ('NC','North Carolina'),
+        ('ND','North Dakota'),
+        ('OH','Ohio'),
+        ('OK','Oklahoma'),
+        ('OR','Oregon'),
+        ('PA','Pennsylvania'),
+        ('RI','Rhode Island'),
+        ('SC','South Carolina'),
+        ('SD','South Dakota'),
+        ('TN','Tennessee'),
+        ('TX','Texas'),
+        ('UT','Utah'),
+        ('VT','Vermont'),
+        ('VA','Virginia'),
+        ('WA','Washington'),
+        ('WV','West Virginia'),
+        ('WI','Wisconsin'),
+        ('WY','Wyoming'),
+        ('PR','Puerto Rico'),
+        ('VI','Virgin Islands'),
+        ('GU','Guam')
+    )
+    zipcode = models.CharField(max_length=10, null=False, blank=False)
+    created_at = models.DateTime
+    modified_at =
+    deadline = 
+
+
+Account
+contractor id r
+tag id r
+name r
+email char field r emailField
+address r
+zipcode r
+notes - textfield o
+created at r
+modified at r
+deadline - datetimefield o
+select account where contractor id = x select account where job id = x
