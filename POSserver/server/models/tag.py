4a791da10024893ffb095bcf2823ca61573e9084
AmyShackles <amyshackles@gmail.com>
2018-09-11 13:37:51 -0700

    Incorporated Zach's tag model

--- POSserver/server/models/tag.py
+++ POSserver/server/models/tag.py
@@ -1,5 +1,11 @@
+
+import uuid
 from django.db import models
 
 
 class Tag(models.Model):
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
