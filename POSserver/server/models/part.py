9460771df5e1eedee827f2783b848580a8feb700
Cole Phillips <nphillips78@gmail.com>
2018-09-12 19:17:19 -0400

    had to delete some migrations and drop some tables and make new migrations

--- POSserver/server/models/part.py
+++ POSserver/server/models/part.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 
 class Part(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    part_name = models.CharField()
+    part_name = models.CharField(max_length=100)
     description = models.TextField()
     cost = models.DecimalField(decimal_places=2, max_digits=5, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
