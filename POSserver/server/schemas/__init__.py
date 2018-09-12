744837f9b128d26727026810811dc8261bb45618
AmyShackles <amyshackles@gmail.com>
2018-09-12 15:19:20 -0700

    Created schema.note and schema.part files
    - Did not create said schemas.
    - Added migrations folder to gitignore and deleted the folder in the hopes that it may be ignored in thefuture.

--- POSserver/server/schemas/__init__.py
+++ POSserver/server/schemas/__init__.py
@@ -3,5 +3,6 @@ from .contractor import Contractor
 from .job import Job
 from .tag import Tag
 from .note import Note
+from .part import Part
 
-__all__ = ["Account", "Contractor", "Job", "Tag", "Note"]
+__all__ = ["Account", "Contractor", "Job", "Tag", "Note", "Part"]
