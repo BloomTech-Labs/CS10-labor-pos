69974bf8db4a2b0b54d8d10365a9832a75b83534
Dre'Sean Williams <williamsdresean@gmail.com>
2018-09-12 15:48:44 -0400

    added more to contractor models

--- POSserver/server/models/contractor.py
+++ POSserver/server/models/contractor.py
@@ -16,3 +16,14 @@ class Contractor(models.User):
     modified_at = models.DateTimeField(auto_now=True, blank=False, null=False)
     premium = models.BooleanField(default=False, blank=False, null=False)
     paid_until = DateTimeField(default=timezone.now, blank=True, null=True)
+
+        def __str__(self): 
+            return (self.first_name, self.last_name, self.business_name, self.email, self.id)
+
+        def make_password(self.password, 11, BCryptSHA256PasswordHasher): 
+            if True:
+                self.password = password
+            else:
+                return none
+
+#### TODO add authorization , pre-save, && validators/sanitation
