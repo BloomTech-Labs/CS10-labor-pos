3ee34e6fb067c6cd278cf81ef6491eb07b7c6c0e
Zach Campbell <beazlebubbles@gmail.com>
2018-09-21 14:29:35 -0600

    add missing balsamiq link

--- client/src/components/clients/clients.js
+++ client/src/components/clients/clients.js
@@ -3,6 +3,8 @@ import { withRouter } from "react-router";
 
 //The clients component renders as a child of the home component and displays a paginated list of client cards
 //on path /clients
+
+//https://balsamiq.cloud/sc1hpyg/po5pcja/r6059
 class Clients extends Component {
   render() {
     return (
