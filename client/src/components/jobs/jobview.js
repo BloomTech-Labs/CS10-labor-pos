ee38ff2e17a69816d9371f0216af07395d0fb06c
Zach Campbell <beazlebubbles@gmail.com>
2018-09-25 17:13:26 -0600

    add basic pagination

--- client/src/components/jobs/jobview.js
+++ client/src/components/jobs/jobview.js
@@ -2,8 +2,25 @@ import React, { Component } from "react";
 import { withRouter } from "react-router";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import { Create, Delete, DoneOutline, ArrowRightAlt } from "@material-ui/icons";
-import { Typography, Grid, Paper, Button } from "@material-ui/core";
+import {
+  Create,
+  Delete,
+  DoneOutline,
+  ArrowRightAlt,
+  NavigateNext,
+  NavigateBefore
+} from "@material-ui/icons";
+import {
+  Typography,
+  Grid,
+  Paper,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from "@material-ui/core";
 import "./jobview.css";
 
 //This component will render as a child of home on the path /jobs/%jobid
@@ -53,6 +70,31 @@ const DETAILED_JOB_BY_ID = gql`
 `;
 
 class JobView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      note_page: 0,
+      part_page: 0,
+      tag_page: 0
+    };
+  }
+
+  handlePageForward = name => event => {
+    event.preventDefault();
+    this.setState({
+      [name]: this.state[name] + 1
+    });
+  };
+
+  handlePageBack = name => event => {
+    event.preventDefault();
+    if (this.state[name] > 0) {
+      this.setState({
+        [name]: this.state[name] - 1
+      });
+    }
+  };
+
   render() {
     return (
       <Query
@@ -61,8 +103,14 @@ class JobView extends Component {
       >
         {({ loading, error, data }) => {
           console.log(data);
+
           let right_content = [];
+          let note_list = [];
+          let part_list = [];
+          let tag_list = [];
+
           if (data.job) {
+            //Build an array of react objects to use as the right-side information display.
             if (data.job.complete) {
               right_content.push(
                 <Typography key={0}>
@@ -83,10 +131,8 @@ class JobView extends Component {
                 <Typography key={1}>Deadline: {data.job.deadline}</Typography>
               );
             }
-
             const created = new Date(data.job.createdAt);
             const modified = new Date(data.job.modifiedAt);
-
             right_content.push(
               <Typography key={2}>
                 Labor/hours worked: {data.job.labor}
@@ -105,7 +151,62 @@ class JobView extends Component {
                 {`${modified.getMonth()}/${modified.getDate()}/${modified.getFullYear()}`}
               </Typography>
             );
+
+            //Build a list of notes to display in the notes area
+            console.log(data.job.noteSet.edges.length);
+            for (let i = 0; i < data.job.noteSet.edges.length; i++) {
+              let current_note = data.job.noteSet.edges[i].node;
+              note_list.push(
+                <ListItem key={i} dense button role={undefined}>
+                  <ListItemText>{current_note.title}</ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete">
+                      <Delete />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            }
+
+            //Build a list of parts to display in the parts area
+            console.log(data.job.partSet.edges.length);
+            for (
+              let i = 7 * this.state.part_page;
+              i < 7 * (this.state.part_page + 1) &&
+              i < data.job.partSet.edges.length;
+              i++
+            ) {
+              console.log("what?!");
+              let current_part = data.job.partSet.edges[i].node;
+              part_list.push(
+                <ListItem key={i} dense button role={undefined}>
+                  <ListItemText>{current_part.name}</ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete">
+                      <Delete />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            }
+
+            //Build a list of tags to display in the tags area
+            console.log(data.job.tagSet.edges.length);
+            for (let i = 0; i < data.job.tagSet.edges.length; i++) {
+              let current_tag = data.job.tagSet.edges[i].node;
+              tag_list.push(
+                <ListItem key={i} dense button role={undefined}>
+                  <ListItemText>{current_tag.name}</ListItemText>
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete">
+                      <Delete />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            }
           }
+
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
           return (
@@ -119,13 +220,17 @@ class JobView extends Component {
                   spacing={24}
                 >
                   <Grid item xs={1}>
-                    <Create />
+                    <IconButton>
+                      <Create />
+                    </IconButton>
                   </Grid>
                   <Grid item xs={10}>
                     <h3>{data.job.name}</h3>
                   </Grid>
                   <Grid item xs={1}>
-                    <Delete />
+                    <IconButton>
+                      <Delete />
+                    </IconButton>
                   </Grid>
                 </Grid>
               </div>
@@ -143,26 +248,52 @@ class JobView extends Component {
                       container
                       direction="row"
                       justify="space-around"
-                      alignItems="center"
+                      alignItems="flex-start"
                       spacing={24}
                     >
                       <Grid item xs={4}>
                         <Button className="job-list-button">
                           Add a new note
                         </Button>
-                        <Paper>note placeholder</Paper>
+                        <Paper>
+                          <List>{note_list}</List>
+                        </Paper>
+                        <IconButton>
+                          <NavigateBefore />
+                        </IconButton>
+                        <IconButton>
+                          <NavigateNext />
+                        </IconButton>
                       </Grid>
                       <Grid item xs={4}>
                         <Button className="job-list-button">
                           Add a new part
                         </Button>
-                        <Paper>part placeholder</Paper>
+                        <Paper>
+                          <List>{part_list}</List>
+                        </Paper>
+                        <IconButton onClick={this.handlePageBack("part_page")}>
+                          <NavigateBefore />
+                        </IconButton>
+                        <IconButton
+                          onClick={this.handlePageForward("part_page")}
+                        >
+                          <NavigateNext />
+                        </IconButton>
                       </Grid>
                       <Grid item xs={4}>
                         <Button className="job-list-button">
                           Add a new tag
                         </Button>
-                        <Paper>tag placeholder</Paper>
+                        <Paper>
+                          <List>{tag_list}</List>
+                        </Paper>
+                        <IconButton>
+                          <NavigateBefore />
+                        </IconButton>
+                        <IconButton>
+                          <NavigateNext />
+                        </IconButton>
                       </Grid>
                     </Grid>
                   </Grid>
