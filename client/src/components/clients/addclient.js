5f1ba7459603cf9de1dcdc3566fb3de3c4b4b5ff
Zach Campbell <beazlebubbles@gmail.com>
2018-09-28 13:30:26 -0600

    add client add and edit wrapper components

--- client/src/components/clients/addclient.js
+++ client/src/components/clients/addclient.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+import ClientForm from "./clientform.js";
 
-//The add client component renders as a child of home on the path
-// /client/create.  It presents the user with a form to submit
-// a mutation for a new client.
+//  The add client component renders as a child of home on the path
+//  /client/create.  It presents the user with a form to submit
+//  a mutation for a new client.
 
-//https://balsamiq.cloud/sc1hpyg/po5pcja/rE2B1
+//  https://balsamiq.cloud/sc1hpyg/po5pcja/rE2B1
 class AddClient extends Component {
   render() {
     return (
       <div>
-        <p>ADDCLIENT PLACEHOLDER</p>
+        <ClientForm mode="create" />
       </div>
     );
   }
