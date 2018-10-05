29aa27f6567efc3627e6fe618cd7eb9419cf631b
Cole Phillips <nphillips78@gmail.com>
2018-10-05 14:45:56 -0400

    perhaps progress?

--- POSserver/server/models/user.py
+++ POSserver/server/models/user.py
@@ -4,7 +4,11 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from django.template.loader import get_template, render_to_string
+from decouple import config
+import sendgrid
+from sendgrid.helpers.mail import *
+from django.http import HttpResponse, HttpResponseRedirect
+# from django.template.loader import get_template, render_to_string
 
 
 if not settings.configured:
@@ -85,11 +89,25 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username} {self.first_name} {self.last_name}"
 
-    # @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-    # def welcome_mail(sender, instance, **kwargs):
-    #     if kwargs['created']:
-    #         model = get_user_model()
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def welcome_mail(sender, instance, **kwargs):
+        if kwargs['created']:
+            model = get_user_model()
+            user_email = instance.email
+            sg = sendgrid.SendGridAPIClient(apikey=config("SENDGRID_API_KEY"))
+            from_email = Email("nphillip78@gmail.com")
+            to_email = user_email
+            subject = "Welcome to contractAlchemy!"
+            content = Content("text/plain", "contractAlchemy is a tool that organizes your clients, jobs, parts, and invoices all in one place. Premium users gain access to all of our features with an unlimited number of records. Our free membership includes access to all features for up to 8 records at a time - 8 clients, 8 jobs,etc. You can upgrade to premium at any time.")
+            mail = Mail(from_email, subject, to_email, content,)
+            response = sg.client.mail.send.post(request_body=mail.get())
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
 
+
+   
+"""
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def welcome_mail(sender, instance, **kwargs):
         if kwargs['created']:
@@ -107,3 +125,4 @@ class User(AbstractUser):
     #         msg.send()
 
     # post_save.connect(welcome_mail, sender=AbstractUser)
+    """
