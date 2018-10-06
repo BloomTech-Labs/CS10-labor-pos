8301b00c4ff57cda8b86987e76705f8dc8f85e89
Zach Campbell <beazlebubbles@gmail.com>
2018-10-06 13:29:00 -0600

    comment noteform

--- client/src/components/notes/noteform.js
+++ client/src/components/notes/noteform.js
@@ -18,6 +18,7 @@ import { styles } from "../material-ui/styles.js";
 
 //  https://balsamiq.cloud/sc1hpyg/po5pcja/r5720
 class NoteForm extends Component {
+  // The state of this component is used to hold the form values
   state = {
     title: "",
     content: "",
@@ -25,15 +26,20 @@ class NoteForm extends Component {
     job: ""
   };
 
+  //  In componentDidMount, we check to see if the component is in edit mode, and if it is we
+  //  load in the current values of the item we are editing.
   componentDidMount = () => {
+    //  Build an edit_note object from the note passed down as a prop.
     if (this.props.mode === "edit") {
       let edit_note = {};
       for (let key in this.props.note) {
         if (this.props.note[key] === null) edit_note[key] = "";
         else edit_note[key] = this.props.note[key];
       }
+      //  If we don't have a client or a job on the item, use the empty string for the value.
       if (!edit_note.client) edit_note.client = { id: "" };
       if (!edit_note.job) edit_note.job = { id: "" };
+      //  Load the edit note into the state.
       this.setState({
         client: edit_note.client.id,
         job: edit_note.job.id,
@@ -43,6 +49,7 @@ class NoteForm extends Component {
     }
   };
 
+  //  Updates the state with form values
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value
@@ -50,24 +57,33 @@ class NoteForm extends Component {
   };
 
   render() {
+    //  Get MaterialUI classes
     const { classes } = this.props;
+    //  Destructure some things from the state
     const { title, content, client, job } = this.state;
+    //  Here, we default some variables to their values that will be used in create mode
     let chosen_mutation = CREATE_NOTE;
     let title_text = "Add Note";
     let button_text = "Create";
+    //  If the component is in edit mode, we change those values to reflect the fact.
     if (this.props.mode === "edit") {
       chosen_mutation = UPDATE_NOTE;
       title_text = `Update ${this.props.note.title}`;
       button_text = "Update";
     }
     return (
+      //  This query gets the names and ids of all clients and jobs to populate the pulldown menus
       <Query query={ALL_CLIENTS_AND_JOBS}>
         {({ loading, error, data }) => {
+          //  Show loading and error messages...
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
+          //  Here, we build the array that will be used for the client pulldown.
           let client_list = [];
           let client_array = data.allClients.edges;
           for (let i = 0; i < client_array.length; i++) {
+            //  This if/else allows us to dynamically show either the businessname or firstname
+            //  / lastname of the client.
             if (client_array[i].node.businessName)
               client_list.push({
                 value: client_array[i].node.id,
@@ -81,6 +97,7 @@ class NoteForm extends Component {
                 }`
               });
           }
+          //  Build the array for the job pulldown.  It's simpler than the client one.
           let job_list = [];
           let job_array = data.allJobs.edges;
           for (let i = 0; i < job_array.length; i++) {
@@ -90,6 +107,8 @@ class NoteForm extends Component {
             });
           }
           return (
+            //  This mutation will submit either a create user or update user mutation
+            //  using the values from our form fields depending on the component mode.
             <Mutation
               mutation={chosen_mutation}
               onCompleted={() => this._confirm()}
@@ -105,18 +124,21 @@ class NoteForm extends Component {
                         job: job,
                         client: client
                       };
-
+                      //  Here, we strip off any empty strings from the variables
                       for (let key in note_variables) {
                         if (note_variables[key] === "") {
                           if (this.props.mode === "edit")
                             delete note_variables[key];
                         }
                       }
+                      //  If we are in edit mode, we need to send up the note id.
                       if (this.props.mode === "edit")
                         note_variables.id = this.props.match.params.id;
+                      //  Send the mutation ...
                       mutateJob({
                         variables: note_variables
                       });
+                      //  And clear local state!
                       this.setState({
                         title: "",
                         content: "",
@@ -125,6 +147,7 @@ class NoteForm extends Component {
                       });
                     }}
                   >
+                    {/*Now for the actual form.  Uses grids for positioning.*/}
                     <Grid container>
                       <Grid item xs={12}>
                         <Typography
@@ -147,6 +170,8 @@ class NoteForm extends Component {
                         />
                       </Grid>
                       <Grid item xs={1} />
+                      {/*This field uses the field class from our styles to
+                      get a distinctive background color.*/}
                       <Grid item xs={10}>
                         <TextField
                           id="field-content"
@@ -164,7 +189,7 @@ class NoteForm extends Component {
                         />
                       </Grid>
                       <Grid item xs={1} />
-
+                      {/*The pulldown form items using the arrays we built above*/}
                       <Grid item xs={6}>
                         <TextField
                           id="field-client"
@@ -214,16 +239,14 @@ class NoteForm extends Component {
                         </TextField>
                       </Grid>
                     </Grid>
-                    <div className="form-bottom-button">
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.padded_button}
-                        type="submit"
-                      >
-                        {button_text}
-                      </Button>
-                    </div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={classes.padded_button}
+                      type="submit"
+                    >
+                      {button_text}
+                    </Button>
                   </form>
                 </div>
               )}
@@ -234,6 +257,7 @@ class NoteForm extends Component {
     );
   }
   _confirm = () => {
+    //  After submission, reload the window to get updated information and go to the notes route.
     window.location.reload();
     this.props.history.push("/notes");
   };
