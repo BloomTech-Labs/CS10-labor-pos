103c971f6655146464b6ffdb86e132d66f415018
Cole Phillips <nphillips78@gmail.com>
2018-10-06 11:34:26 -0400

    setting up invoice emails from user to client

---
+++ POSserver/send/invoice.py
@@ -0,0 +1,40 @@
+import json
+import sendgrid
+
+from decouple import config
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.conf import settings
+from POSserver.POSserver.views import GeneratePDF
+from POSserver.server.models.job import Job
+from sendgrid.helpers.mail import *
+
+
+def send_email(req):
+    body = json.loads(req.body.decode('utf-8'))
+    user_name = body['userName']
+    user_email = body['userEmail']
+    job_name = body['jobName']
+    order_id = body['jobId']
+    client_name = body['clientFirstName']
+    client_email = body['clientEmail']
+
+    sg = sendgrid.SendGridAPIClient(apikey=config.('SENDGRID_API_KEY'))
+
+    from_email = Email(user_email)
+    to_email = Email(client_email)
+    subject = f'New invoice from {user_name}'
+    attachment = Attachment()
+    attachment.type = 'application/pdf'
+
+  content = Content(
+    'text/html',
+    f'''
+    <p>Hi <b>{client_name}</b></p>
+    <p>You have a new invoice from <b>{user_name}</b>. To view your invoice, open the attached PDF or view <insert link>.</p> 
+    '''
+  )
+
+  mail = Mail(from_email, subject, to_email, content)
+  response = sg.client.mail.send.post(request_body=mail.get())
+ 
