1a21201137086fe701a1b40a76b7aaaa1be5adc0
Cole Phillips <nphillips78@gmail.com>
2018-10-09 19:33:27 -0400

    working on payment models

--- POSserver/payment/models.py
+++ POSserver/payment/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from decouple import config
+import stripe
 
 # Create your models here.
+
+
+class Token(models.model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.CharField()
+    livemode = models.BooleanField(default=False)
+    type = models.CharField()
+    used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{id}"
+
+
+class Charge(models.model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+
+
+
+
+
+
+
+
+
