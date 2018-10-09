cd364dacddd9b9690122b94f0447bc8187ac4ff0
Cole Phillips <nphillips78@gmail.com>
2018-10-09 19:38:26 -0400

    added models for stripe stuff

--- POSserver/payment/models.py
+++ POSserver/payment/models.py
@@ -8,9 +8,9 @@ import stripe
 
 class Token(models.model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created = models.CharField()
+    created = models.CharField(default="")
     livemode = models.BooleanField(default=False)
-    type = models.CharField()
+    type = models.CharField(default="")
     used = models.BooleanField(default=False)
 
     def __str__(self):
@@ -19,7 +19,19 @@ class Token(models.model):
 
 class Charge(models.model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+    amount = models.CharField(default="")
+    captured = models.BooleanField(default=False)
+    created = models.CharField(default="")
+    currency = models.CharField(default="")
+    description = models.CharField(max_length=22, default="")
+    status = models.CharField(default="")
+
+    def __str__(self):
+        return f"{status}"
+
+
+
+
 
 
 
