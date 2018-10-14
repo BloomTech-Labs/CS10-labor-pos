8d41ef5fb648eb710b35a64e3ac441ed595671ad
AmyShackles <amyshackles@gmail.com>
2018-10-13 17:07:12 -0700

    Removes print statements from views.py

--- POSserver/POSserver/views.py
+++ POSserver/POSserver/views.py
@@ -15,19 +15,12 @@ class GeneratePDF(View):
         req = json.loads(request.body)
         job = Job.objects.get(pk=from_global_id(req["job"])[1])
         user = User.objects.get(pk=job.user_id)
-        print(user)
         client = Client.objects.filter(pk=job.client_id).get()
-        print(client.first_name)
-        # parts = Part.objects.all()
         job_parts = Part.objects.filter(job_id=job).values()
-        print(job_parts)
         total = 0
         for part in job_parts:
             total = total + part["cost"]
         total = total + job.labor
-        # print(parts.__dict__)
-        print(job)
-        # client.name = "%s %s" % (client.first_name, client.last_name)
         context = {
             "invoice_number": job.id,
             "today": datetime.date.today(),
@@ -53,14 +46,14 @@ class GeneratePDF(View):
 
 
 def checkout(request):
-        if request.method == "POST":
+    if request.method == "POST":
 
-            token = request.form["stripeToken"]
+        token = request.form["stripeToken"]
 
-            checkout = stripe.Charge.create(
-                amount=request.POST["amount"],
-                currency="usd",
-                description=request.POST["description"],
-                source=token,
-            )
-            return HttpResponse(checkout)
+        checkout = stripe.Charge.create(
+            amount=request.POST["amount"],
+            currency="usd",
+            description=request.POST["description"],
+            source=token,
+        )
+        return HttpResponse(checkout)
