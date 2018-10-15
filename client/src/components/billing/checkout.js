81437c19f80a4552e8ea141d7e0cc6ea23dec241
Cole Phillips <nphillips78@gmail.com>
2018-10-15 17:11:12 -0400

    added back the paywall code I'd accidentally deleted

--- client/src/components/billing/checkout.js
+++ client/src/components/billing/checkout.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 import {
   FormControlLabel,
   Checkbox,
   Typography,
   Card
 } from "@material-ui/core";
-
+import { AUTH_TOKEN } from "../../constants.js";
 
 class Checkout extends Component {
   state = {
@@ -25,45 +26,54 @@ class Checkout extends Component {
     });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.setState({ card_errors: '', resp_message: '' });
-    /*
-    Within the context of Elements, this call to createToken knows which
-    Element to tokenize, since there's only one in this group.
-    */
+  getStripeToken = token => {
+    let apiURI = 'http://localhost:8000/graphql/';
 
-    // stripe's checkout plugin receives card information, creates a stripe customer, tokenizes the information, sends the token back to our backend so that our backend can create the charge
-    return this.props.stripe
-      .createToken({ type: 'card', name: ' ' })
-      .then(result => {
-        if (result.error) {
-          console.log('THERE IS AN ERROR IN YOUR FORM', result.error);
-          return this.setState({ card_errors: result.error.message });
-        } else {
-          console.log(
-            'Received Stripe token ---> SENDING TO SERVER: ',
-            result.token
-          );
-          let formData = new FormData();
-          formData.append('description', 'Premium');
-          formData.append('currency', 'usd');
-          formData.append('amount', 9999);
-          formData.append('source', result.token.id);
-          return fetch('https://bestpos.netlify.com/createcharge/', {
-            method: 'POST',
-            headers: {
-              accept: 'application/graphql'
-            },
-            body: formData
-          })
-            .then(resp => resp.json())
-            .then(json => this.setState({ resp_message: json.message }));
+    const request = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/graphql'
+      },
+      url: apiURI,
+      data: { token, jwt: localStorage.getItem('token') },
+      body: JSON.stringify({ query: '{ token: { id } }' })
+    };
+
+    axios({
+      url: process.env.REACT_APP_ENDPOINT,
+      method: 'post',
+      headers: {
+        Authorization: 'JWT ' + localStorage.getItem(AUTH_TOKEN),
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({
+        operationName: null,
+        query: `mutation updateUser($id: ID, $subscription: String) {
+            updateUser(id: $id, subscription: $subscription) {
+              user {
+                id
+                premium
+            }
+          }
+        }`,
+        variables: {
+          id: localStorage.getItem('USER_ID'),
+          subscription: this.state.subscriptionType
         }
-      });
+      })
+    })
+      .then(res => {
+        console.log(res.data.data.updateUser.user.premium);
+        localStorage.setItem(
+          'USER_PREMIUM',
+          res.data.data.updateUser.user.premium
+        );
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
+    // stripe's checkout plugin receives card information, creates a stripe customer, tokenizes the information, sends the token back to our backend so that our backend can create the charge
     return <div>
         <Card>
           <Typography paragraph>
@@ -75,14 +85,14 @@ class Checkout extends Component {
         <Card>
           <Typography paragraph>
             {
-              'Free users use the default theme and have access to all other features but a limit of 8 of each item at one time - 8 clients, 8 jobs, etc.'
+              "Free users use the default theme and have access to all other features but a limit of 8 of each item at one time - 8 clients, 8 jobs, etc."
             }
           </Typography>
         </Card>
         <Card>
           <Typography paragraph>
             {
-              'Premium users have access to all features with an unlimited number of jobs, clients, and so on.  Premium users also have multiple theme choices and the ability to create custom themes.We offer our premium subscription at two different rates - a monthly fee of 99¢ or a yearly fee of $9.99.'
+              "Premium users have access to all features with an unlimited number of jobs, clients, and so on.  Premium users also have multiple theme choices and the ability to create custom themes.We offer our premium subscription at two different rates - a monthly fee of 99¢ or a yearly fee of $9.99."
             }
           </Typography>
         </Card>
@@ -93,8 +103,7 @@ class Checkout extends Component {
             }
           </Typography>
         </Card>
-
-{/*checkboxes allow user to select which premium plan they want to pay for, then sets the amount in the stripe form*/}
+        {/*checkboxes allow user to select which premium plan they want to pay for, then sets the amount in the stripe form*/}
         <React.Fragment>
           <FormControlLabel control={<Checkbox price={999} name="subscription" onClick={this.setSubscriptionType} value="year" type="radio" color="secondary" />} label="Yearly Subscription - $9.99" />
           <FormControlLabel control={<Checkbox price={99} name="subscription" onClick={this.setSubscriptionType} value="month" type="radio" color="secondary" />} label="Monthly Subscription - 99¢" />
