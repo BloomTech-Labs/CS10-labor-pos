4e4d6875b8b759be503bd38a392eb05e741d6395
AmyShackles <amyshackles@gmail.com>
2018-10-17 17:48:28 -0700

    Changed back to BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
-import { createBrowserHistory } from "history";
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_ENDPOINT,
@@ -32,10 +31,8 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={history}>
+  <Router>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
