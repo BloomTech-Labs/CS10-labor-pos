c6932950e976f89ea2c5d9d80c71b0bf3e3e64c1
Amy Shackles <amyshackles@gmail.com>
2020-11-12 10:54:37 -0800

    Adds django-rest-framework endpoint to view list of usernames

--- POSserver/POSserver/urls.py
+++ POSserver/POSserver/urls.py
@@ -15,16 +15,31 @@ Including another URLconf
 """
 # from django.conf.urls import url
 from django.contrib import admin
+from server.models import User
+from rest_framework import routers, serializers, viewsets
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from .views import GeneratePDF
 from payment.views import checkout
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("admin/", admin.site.urls),
     re_path(r"^graphql/$", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     re_path(r"^graphql/pdf/$", csrf_exempt(GeneratePDF.as_view())),
     re_path(r"^graphql/create-charge/$", csrf_exempt(checkout)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
